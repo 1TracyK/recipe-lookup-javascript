b44b9837845180ed624512f486995f33d06c07b5
Tracy L. Kimbrough <Ladyjettt@gmail.com>
2023-02-21 15:16:19 -0600

    test commit

---
+++ food-loolup.js
@@ -0,0 +1,8 @@
+https-github.com-1TracyK-recipe-lookup-javascript
+
+const form = document.querySelector('.js-food-form');
+const inputField = document.querySelector('.js-food-input');
+const foodContainer = document.querySelector('.js-meal-list-container');
+
+// https://www.themealdb.com/api/json/v1/1/search.php?s=Pie
+
